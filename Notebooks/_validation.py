bf51c119c2037fc4b84faef0a2145091a45fccd2
Vincent N. <vincent.namy@edu.ge.ch>
2025-12-24 00:11:17 +0100

    validation : ajout validation de quizz

--- Notebooks/_validation.py
+++ Notebooks/_validation.py
@@ -5,12 +5,12 @@ from basthon.autoeval import (
     ValidateFunctionPretty,
     validationclass,
 )
+from typing import Any
 import time
 import sys
+from js import document, Reflect
 
-test_a_42 = ValidateVariables({"a": 42})
-
-
+##### Classes custom
 @validationclass
 class LitLaConsigne:
     def __init__(self):
@@ -26,6 +26,52 @@ class LitLaConsigne:
             print("As-tu vraiment pris le temps de lire la consigne ?", file=sys.stderr)
             return False
 
+@validationclass
+class ValidateQuiz(ValidateSequences[str, Any]):
+    """A class to validate html form checkbox quiz. """
 
+    def __init__(self, ids_and_values: dict[str, Any], **kwargs):
+        """
+        :param ids_and_values: a dict with quiz Ids (<form id="">)as key
+            and list of correct answers values as values to test against checked answers
+            (<label><input type="checkbox" name="option" value="A"> A) Réponse A</label><br>)
+            defined in the user namespace (top-level).
+        :param **kwargs: passed to parent constructor.
+        """
+        for key in ids_and_values.keys(): # for list typing of potential single answer
+            if not isinstance(ids_and_values[key], list):
+                ids_and_values[key] = [ids_and_values[key]]
+        super().__init__(ids_and_values.keys(), ids_and_values.values(), **kwargs)
+
+    def compute_result(self, id: str, precomputed_data: Any) -> Any:
+        form = document.getElementById(id)
+        inputs = form.querySelectorAll('input[type="checkbox"]')
+
+        rep =[item.value for item in inputs if item.checked]
+        return rep
+
+    def handle_failure(self, name: str, target: Any, value: Any) -> bool:
+        print(
+            f"Mauvaise(s) réponse(s), essaie encore.",
+            file=sys.stderr,
+        )
+        return False
+
+    def handle_exception(self, exc: Exception, name: str, target: Any) -> bool:
+        print(
+            f"Erreur Il y a un problème avec : '{name}'",
+            file=sys.stderr,
+        )
+        return False
+
+    def handle_full_success(self):
+        print("👏 Bravo, bonne(s) réponse(s) !")
+            
+            
+#####  Réponses exos
+# Divers
+test_a_42 = ValidateVariables({"a": 42})
 continuer_si_10 = LitLaConsigne()
 
+# Notebook 1IN - 5a
+test_quiz1 = ValidateQuiz({"quiz1": ["C"]})
