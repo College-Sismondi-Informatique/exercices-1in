c042175d6f283bab69342f975d7164f456633dc2
edba-coding <ebasilico@protonmail.com>
2026-03-12 14:29:57 +0100

    _tentative de correction validation

--- Notebooks/_validation.py
+++ Notebooks/_validation.py
@@ -4,55 +4,30 @@ from basthon.autoeval import (
     ValidateFunction,
     ValidateFunctionPretty,
     validationclass,
-    ValidateSequences,
+    ValidateSequences
 )
 from typing import Any
 import time
 import sys
-
-# Essayer d'importer les objets JS, sinon fournir des valeurs None
-try:
-    from js import document, Reflect, Jupyter  # type: ignore
-except Exception:
-    document = None
-    Reflect = None
-    Jupyter = None
+from js import document, Reflect, Jupyter
 
 codes_dict = {}
-lastCode = ""
+lastCode = ''
 
 
 def store_all_cells_content():
     global codes_dict
-    if Jupyter is None or not hasattr(Jupyter, "notebook"):
-        return
     for i, cell in enumerate(Jupyter.notebook.get_cells()):
-        # protection si l'API est différente
-        cm = getattr(cell, "code_mirror", None)
-        if cm is not None and hasattr(cm, "getValue"):
-            codes_dict[i] = cm.getValue()
-
+            codes_dict[i] = cell.code_mirror.getValue()
 
 def store_cell_content():
     global codes_dict
-    if Jupyter is None or not hasattr(Jupyter, "notebook"):
-        return
-    idx = Jupyter.notebook.get_selected_index()
-    cell = Jupyter.notebook.get_selected_cell()
-    cm = getattr(cell, "code_mirror", None)
-    if cm is not None and hasattr(cm, "getValue"):
-        codes_dict[idx] = cm.getValue()
-
-
+    codes_dict[Jupyter.notebook.get_selected_index()] = Jupyter.notebook.get_selected_cell().code_mirror.getValue()
+    
 def restore_all_cells_content():
-    if Jupyter is None or not hasattr(Jupyter, "notebook"):
-        return
     for i, cell in enumerate(Jupyter.notebook.get_cells()):
         if i in codes_dict:
-            cm = getattr(cell, "code_mirror", None)
-            if cm is not None and hasattr(cm, "setValue"):
-                cm.setValue(codes_dict[i])
-
+            cell.code_mirror.setValue(codes_dict[i])
 
 ##### Classes custom
 @validationclass
@@ -62,7 +37,7 @@ class LitLaConsigne:
         self._start = time.time()
 
     def __call__(self):
-        # au moins 10 secondes (ajusté pour test rapide)
+        # au moins 5 minutes se sont écoulées
         if time.time() - self._start > 10:
             print("Bravo, tu as pris le temps de lire la consigne !")
             return True
@@ -70,96 +45,87 @@ class LitLaConsigne:
             print("As-tu vraiment pris le temps de lire la consigne ?", file=sys.stderr)
             return False
 
-
 @validationclass
 class ValidateQuiz(ValidateSequences[str, Any]):
-    """A class to validate html form checkbox quiz."""
+    """A class to validate html form checkbox quiz. """
 
-    def __init__(self, ids_and_values: dict[str, Any] | str, **kwargs):
-        # Si on reçoit un str, on récupère les bonnes réponses depuis le DOM
-        if isinstance(ids_and_values, str):
-            if document is None:
-                ids_and_values = {ids_and_values: []}
-            else:
-                el = document.getElementById(ids_and_values)
-                if el is None:
-                    ids_and_values = {ids_and_values: []}
-                else:
-                    inputs = el.querySelectorAll('input[type="checkbox"]')
-                    rep = [item.name for item in inputs if getattr(item, "value", None) == "correct"]
-                    ids_and_values = {ids_and_values: rep}
-
-        # Forcer la liste pour chaque valeur
-        for k in list(ids_and_values.keys()):
-            if not isinstance(ids_and_values[k], list):
-                ids_and_values[k] = [ids_and_values[k]]
-
-        # ValidateSequences attend des itérables parallèles : on lui passe lists
-        keys = list(ids_and_values.keys())
-        values = [ids_and_values[k] for k in keys]
-        super().__init__(keys, values, **kwargs)
+    def __init__(self, ids_and_values: dict[str, Any], **kwargs):
+        """
+        :param ids_and_values: a dict with quiz Ids (<form id="">)as key
+            and list of correct answers values as values to test against checked answers
+            (<label><input type="checkbox" name="C" value="correct"> C) Réponse C</label><br>)
+            defined in the user namespace (top-level).
+        :param ids_and_values: alternatively, a simple str with the id. Answer will then be searched in the source page
+        :param **kwargs: passed to parent constructor.
+        """
+        if isinstance(ids_and_values, str): # If no answer is specified, find correct answer in HTML "value" parameter
+            inputs = document.getElementById(ids_and_values).querySelectorAll('input[type="checkbox"]')
+            rep =[item.name for item in inputs if item.value=="correct"]
+            ids_and_values = {ids_and_values:rep}
+            
+        for key in ids_and_values.keys():  # force list typing of potential single answer            
+            if not isinstance(ids_and_values[key], list):
+                ids_and_values[key] = [ids_and_values[key]]
+        super().__init__(ids_and_values.keys(), ids_and_values.values(), **kwargs)
 
     def compute_result(self, id: str, precomputed_data: Any) -> Any:
-        if document is None:
-            return []
-        el = document.getElementById(id)
-        if el is None:
-            return []
-        inputs = el.querySelectorAll('input[type="checkbox"]')
-        rep = [item.name for item in inputs if getattr(item, "checked", False)]
+        # Return list of names of checked items
+        inputs = document.getElementById(id).querySelectorAll('input[type="checkbox"]')
+        rep =[item.name for item in inputs if item.checked]
         return rep
 
     def handle_failure(self, name: str, target: Any, value: Any) -> bool:
-        print("Mauvaise(s) réponse(s), essaie encore.", file=sys.stderr)
+        print(
+            f"Mauvaise(s) réponse(s), essaie encore.",
+            file=sys.stderr,
+        )
         return False
 
     def handle_exception(self, exc: Exception, name: str, target: Any) -> bool:
-        print(f"Erreur Il y a un problème avec : '{name}'", file=sys.stderr)
+        print(
+            f"Erreur Il y a un problème avec : '{name}'",
+            file=sys.stderr,
+        )
         return False
 
     def handle_full_success(self):
         print("👏 Bravo, bonne(s) réponse(s) !")
 
-
 @validationclass
-class ValidateTextInput(ValidateSequences[str, str]):
-    """A class to validate html text inputs."""
+class ValidateTextInput(ValidateSequences[str, str]): # TODO : Refactor ValidateTextInput and ValidateQuiz in one ValidateForm to check every input and signal error
+    """A class to validate html form checkbox quiz. """
 
-    def __init__(self, ids_and_values: dict[str, str] | str, **kwargs):
-        if isinstance(ids_and_values, str):
-            if document is None:
-                ids_and_values = {ids_and_values: []}
-            else:
-                el = document.getElementById(ids_and_values)
-                if el is None:
-                    ids_and_values = {ids_and_values: []}
-                else:
-                    inputs = el.querySelectorAll('input[type="text"]')
-                    ids_and_values = {ids_and_values: [i.id for i in inputs]}
-
-        for k in list(ids_and_values.keys()):
-            if not isinstance(ids_and_values[k], list):
-                ids_and_values[k] = [ids_and_values[k]]
-
-        keys = list(ids_and_values.keys())
-        values = [ids_and_values[k] for k in keys]
-        super().__init__(keys, values, **kwargs)
+    def __init__(self, ids_and_values: dict[str, str], **kwargs):
+        """
+        :param ids_and_values: a dict with input Ids (<form id="">)as key
+            and correct answer as values to test against checked answers
+            (<label>Ma réponse : <input type="text" id="12" value=""></label><br>)
+            defined in the user namespace (top-level).
+        :param ids_and_values: alternatively, a simple str with the id. Answer will then be searched in the source page
+        :param **kwargs: passed to parent constructor.
+        """
+        if isinstance(ids_and_values, str): # If no answer is specified, find correct answer in HTML "value" parameter
+            inputs = document.getElementById(ids_and_values).querySelectorAll('input[type="text"]')
+            ids_and_values = {ids_and_values:[i.id for i in inputs]}
+        super().__init__(ids_and_values.keys(), ids_and_values.values(), **kwargs)
 
     def compute_result(self, id: str, precomputed_data: Any) -> Any:
-        if document is None:
-            return []
-        el = document.getElementById(id)
-        if el is None:
-            return []
-        inputs = el.querySelectorAll('input[type="text"]')
-        return [getattr(i, "value", "") for i in inputs]
+        # Return value of input field
+        inputs = document.getElementById(id).querySelectorAll('input[type="text"]')
+        return [i.value for i in inputs] 
 
     def handle_failure(self, name: str, target: Any, value: Any) -> bool:
-        print("Mauvaise(s) réponse(s), essaie encore.", file=sys.stderr)
+        print(
+            f"Mauvaise(s) réponse(s), essaie encore.",
+            file=sys.stderr,
+        )
         return False
 
     def handle_exception(self, exc: Exception, name: str, target: Any) -> bool:
-        print(f"Erreur Il y a un problème avec : '{name}'", file=sys.stderr)
+        print(
+            f"Erreur Il y a un problème avec : '{name}'",
+            file=sys.stderr,
+        )
         return False
 
     def handle_full_success(self):
@@ -167,84 +133,42 @@ class ValidateTextInput(ValidateSequences[str, str]):
 
 
 def remove_shortcuts():
-    keep_shortcut = {
-        "jupyter-notebook:run-cell",
-        "jupyter-notebook:run-cell-and-select-next",
-        "jupyter-notebook:save-notebook",
-        "jupyter-notebook:select-previous-cell",
-        "jupyter-notebook:select-next-cell",
-        "jupyter-notebook:enter-edit-mode",
-    }
+    keep_shortcut = ["jupyter-notebook:run-cell",
+                     "jupyter-notebook:run-cell-and-select-next",
+                     "jupyter-notebook:save-notebook",
+                     "jupyter-notebook:select-previous-cell",
+                     "jupyter-notebook:select-next-cell",
+                     "jupyter-notebook:enter-edit-mode"]
+    
 
-    if Jupyter is None or not hasattr(Jupyter, "keyboard_manager"):
-        return
-
-    km = Jupyter.keyboard_manager
-
-    # Command mode shortcuts
-    cmd_shortcuts = getattr(km, "command_shortcuts", None)
-    if cmd_shortcuts is not None and hasattr(cmd_shortcuts, "_shortcuts"):
-        try:
-            items = cmd_shortcuts._shortcuts.to_py()
-        except Exception:
-            try:
-                items = dict(cmd_shortcuts._shortcuts)
-            except Exception:
-                items = {}
-        for k, v in list(items.items()):
-            if (v not in keep_shortcut) or len(k) == 1:
-                if isinstance(v, dict):
-                    for k1 in v:
-                        try:
-                            cmd_shortcuts.remove_shortcut(f"{k},{k1}")
-                        except Exception:
-                            pass
-                else:
-                    try:
-                        cmd_shortcuts.remove_shortcut(k)
-                    except Exception:
-                        pass
-
-    # Edit mode shortcuts
-    edit_shortcuts = getattr(km, "edit_shortcuts", None)
-    if edit_shortcuts is not None and hasattr(edit_shortcuts, "_shortcuts"):
-        try:
-            items = edit_shortcuts._shortcuts.to_py()
-        except Exception:
-            try:
-                items = dict(edit_shortcuts._shortcuts)
-            except Exception:
-                items = {}
-        for k, v in list(items.items()):
-            if (v not in keep_shortcut) or len(k) == 1:
-                if isinstance(v, dict):
-                    for k1 in v:
-                        try:
-                            edit_shortcuts.remove_shortcut(f"{k},{k1}")
-                        except Exception:
-                            pass
-                else:
-                    try:
-                        edit_shortcuts.remove_shortcut(k)
-                    except Exception:
-                        pass
-
-
-# Appel sûr de la suppression des shortcuts
-try:
-    remove_shortcuts()
-except Exception:
-    # Ne pas planter si l'API n'est pas présente
-    pass
-
-
-#####  Réponses exos (exemples et tests)
+    for k,v in Jupyter.keyboard_manager.command_shortcuts._shortcuts.to_py().items():
+        if v not in keep_shortcut or len(k) == 1:
+            if isinstance(v,dict):
+                for k1 in v:
+                    Jupyter.keyboard_manager.command_shortcuts.remove_shortcut(f"{k},{k1}")
+            else:
+                Jupyter.keyboard_manager.command_shortcuts.remove_shortcut(k)
+                
+    for k,v in Jupyter.keyboard_manager.edit_shortcuts._shortcuts.to_py().items():
+        if v not in keep_shortcut or len(k) == 1:
+            if isinstance(v, dict):
+                for k1 in v:
+                    Jupyter.keyboard_manager.edit_shortcuts.remove_shortcut(f"{k},{k1}")
+            else:
+                Jupyter.keyboard_manager.edit_shortcuts.remove_shortcut(k)
+                
+remove_shortcuts()
+                        
+            
+#####  Réponses exos
+### Exemples
 # test_a_42 = ValidateVariables({"a": 42})
 # continuer_si_10 = LitLaConsigne()
 # test_quiz_exemple = ValidateQuiz({"quiz1": ["C"], "quiz2": ["A","B"]})
-# test_quiz_exemple_2 = ValidateQuiz("quiz1")
+# test_quiz_exemple_2 = ValidateQuiz("quiz1") # same but with answer in source page
 # test_text_input_1 = ValidateTextInput({"question1.1": "42"})
-# test_text_input_2 = ValidateTextInput("question1.1")
+# test_text_input_2 = ValidateTextInput("question1.1") # same but with answer in source page
+
 
 # Notebook 5a
 test_5a_3 = ValidateVariables({"A": 121})
