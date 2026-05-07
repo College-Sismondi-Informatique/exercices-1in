8628a3622581f9161bc6ba68163eecc6dd94eef0
pignouf305 <32131211+pignouf305@users.noreply.github.com>
2026-05-07 16:15:15 +0200

    Glouton : ajout NB

--- Algorithmique/Glouton/distributeur_corrige.py
+++ Algorithmique/Glouton/distributeur_corrige.py
@@ -1,11 +1,10 @@
 from distributeur import nombre_billets, billet_suivant
 
 somme = 3870
-somme_restante = somme
 
 for _ in range(nombre_billets):
     billet = billet_suivant()
-    while somme_restante >= billet:
+    while somme >= billet:
         print(billet)
-        somme_restante = somme_restante-billet
+        somme = somme-billet
         
